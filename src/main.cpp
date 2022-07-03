3cf3a3a750fbe5006c27451bd2050cd096f7c429
Anthony Le <aznthonyle45@gmail.com>
2022-07-03 15:02:55 -0700

    Library installed via PIO, and successfully tested in reading data from the sensor.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,33 @@
 #include <Arduino.h>
-#include <DallasTemperature.h>
-#include <dht_nonblocking.h>
+#include <DHT.h>
+
+// Define pin location and sensor type
+#define DHTPIN 0
+#define DHTTYPE DHT11
+
+float temp, humid;
+
+// Instantiate the sensor object
+DHT dht_sensor(DHTPIN, DHTTYPE);
 
 void setup() {
-  // put your setup code here, to run once:
+
+  // Initialize the sensor and Serial baud rate
+  Serial.begin(9600);
+  dht_sensor.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  // Wait 1 second before next read
+  delay(1000);
+
+  // Get the Temperature and Humidity
+  temp = dht_sensor.readTemperature(true);
+  humid = dht_sensor.readHumidity();
+
+  // Output the data
+  Serial.printf("\nHumidity: %.2f\n", humid);
+  Serial.printf("Temperature: %.2f\n", temp);
+
 }
